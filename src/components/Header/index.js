37236fb81e370b42dc7ec3522f0cfa45ee77534d
Andy Marks <vampwillow@gmail.com>
2018-04-05 16:50:03 +1000

    Trying to get MDL styles working in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,13 @@ const Header = () => (
     <div className={`mdl-layout__header-row ${s.row}`}>
       <div className={`mdl-layout-title ${s.title}`} to="/">
         female speakers @ tech conferences
-          </div>
-      <div className="mdl-layout-spacer"></div>
+      </div>
+    </div>
+    <div className={`mdl-layout__header-row ${s.row}`}>
+      As a generalist software geek in Australia/NZ, there is not a lot of choice when it comes to conferences. One advantage of the limited menu we have is that it is relatively easy to track trends of the available conferences over time and one such trend I am particularly interested in is how many female speakers get an opportunity to present at these conferences.
+
+      <strong>Disclaimer:</strong><em>Gender presence is only one form of diversity to consider and the approach I’ve used is unscientific and assuming a <a href="https://en.wikipedia.org/wiki/Gender_binary">gender binary</a> world, but I hope you find this information useful even with the shortcomings in my method.</em>
+
     </div>
   </header>
 )
