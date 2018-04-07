4259e1486e6cca9f22bd562a647b8f061e98fedb
Andy Marks <vampwillow@gmail.com>
2018-04-07 10:32:12 +1000

    Ported across old styles

--- src/html.js
+++ src/html.js
@@ -29,6 +29,13 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,500italic,700,700italic"></link>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+          <link rel="stylesheet" href="https://npmcdn.com/react-bootstrap-table/dist/react-bootstrap-table-all.min.css"></link>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" crossOrigin="anonymous"></link>
+          <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"></link>
+          <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>        
+
           {this.props.headComponents}
           {css}
         </head>
