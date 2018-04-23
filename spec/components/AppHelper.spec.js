135ae69f1058f0a26b1ce509e899b7363154991a
Andy Marks <vampwillow@gmail.com>
2018-04-24 09:17:54 +1000

    Sorting used to be done by table - now by helper

--- spec/components/AppHelper.spec.js
+++ spec/components/AppHelper.spec.js
@@ -21,5 +21,12 @@ describe("The AppHelper module", function() {
         const confListWithMissingFields = helper.completeMissingFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }]);
         expect(confListWithMissingFields[0].node.diversityPercentage).toEqual(.3);
     });
+    
+    it("can returns confs in order of descending diversityPercentage", function() {
+        const confListWithMissingFields = helper.completeMissingFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }, { node: { totalSpeakers: 10, numberOfWomen: 2 } }, { node: { totalSpeakers: 10, numberOfWomen: 4 } }]);
+        expect(confListWithMissingFields[0].node.diversityPercentage).toEqual(.4);
+        expect(confListWithMissingFields[1].node.diversityPercentage).toEqual(.3);
+        expect(confListWithMissingFields[2].node.diversityPercentage).toEqual(.2);
+    });
 
 });
