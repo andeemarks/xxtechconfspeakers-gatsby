e8534fab2557c46b04220e1039b7f2f9e68c3a8c
Andy Marks <vampwillow@gmail.com>
2018-08-30 16:13:45 +1000

    Test restructure to show common test cases

--- spec/components/AppHelper.spec.js
+++ spec/components/AppHelper.spec.js
@@ -8,45 +8,52 @@ describe("The AppHelper module", function() {
         helper = new AppHelper();
     });
     
-    it("can leaves a empty conf list unchanged", function() {
-        expect(helper.addDerivedFields([])).toEqual([]);
+    describe("when assigning ranks", function() {
+
+        it("can derive the index field with unique diversityPercentage values", function() {
+            const confListWithMissingFields = helper.assignRanks(helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }, { node: { totalSpeakers: 10, numberOfWomen: 2 } }, { node: { totalSpeakers: 10, numberOfWomen: 4 } }]));
+            expect(confListWithMissingFields[0].node.index).toEqual(1);
+            expect(confListWithMissingFields[1].node.index).toEqual(2);
+            expect(confListWithMissingFields[2].node.index).toEqual(3);
+        });
+        
+        it("can derive the index field with duplicate diversityPercentage values", function() {
+            const confListWithMissingFields = helper.assignRanks(helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }, { node: { totalSpeakers: 5, numberOfWomen: 2 } }, { node: { totalSpeakers: 10, numberOfWomen: 4 } }]));
+            expect(confListWithMissingFields[0].node.index).toEqual(1);
+            expect(confListWithMissingFields[1].node.index).toEqual("");
+            expect(confListWithMissingFields[2].node.index).toEqual(3);
+        });
+        
+        it("can derive the index field with similar diversityPercentage values that appear duplicate with rounding", function() {
+            const confListWithMissingFields = helper.assignRanks(helper.addDerivedFields([{ node: { totalSpeakers: 21, numberOfWomen: 11 } }, { node: { totalSpeakers: 23, numberOfWomen: 12 } }]));
+            expect(confListWithMissingFields[0].node.index).toEqual(1);
+            expect(confListWithMissingFields[1].node.index).toEqual("");
+        });
+
     });
+
+    describe("when adding derived fields", function() {
     
-    it("can derive the index field with unique diversityPercentage values", function() {
-        const confListWithMissingFields = helper.assignRanks(helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }, { node: { totalSpeakers: 10, numberOfWomen: 2 } }, { node: { totalSpeakers: 10, numberOfWomen: 4 } }]));
-        expect(confListWithMissingFields[0].node.index).toEqual(1);
-        expect(confListWithMissingFields[1].node.index).toEqual(2);
-        expect(confListWithMissingFields[2].node.index).toEqual(3);
-    });
+        it("can leaves a empty conf list unchanged", function() {
+            expect(helper.addDerivedFields([])).toEqual([]);
+        });
     
-    it("can derive the index field with duplicate diversityPercentage values", function() {
-        const confListWithMissingFields = helper.assignRanks(helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }, { node: { totalSpeakers: 5, numberOfWomen: 2 } }, { node: { totalSpeakers: 10, numberOfWomen: 4 } }]));
-        expect(confListWithMissingFields[0].node.index).toEqual(1);
-        expect(confListWithMissingFields[1].node.index).toEqual("");
-        expect(confListWithMissingFields[2].node.index).toEqual(3);
-    });
-    
-    it("can derive the index field with similar diversityPercentage values that appear duplicate with rounding", function() {
-        const confListWithMissingFields = helper.assignRanks(helper.addDerivedFields([{ node: { totalSpeakers: 21, numberOfWomen: 11 } }, { node: { totalSpeakers: 23, numberOfWomen: 12 } }]));
-        expect(confListWithMissingFields[0].node.index).toEqual(1);
-        expect(confListWithMissingFields[1].node.index).toEqual("");
-    });
-    
-    it("can derive the numberOfMen field", function() {
-        const confListWithMissingFields = helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }]);
-        expect(confListWithMissingFields[0].node.numberOfMen).toEqual(7);
-    });
-    
-    it("can derive the diversityPercentage field", function() {
-        const confListWithMissingFields = helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }]);
-        expect(confListWithMissingFields[0].node.diversityPercentage).toEqual(.3);
-    });
-    
-    it("can returns confs in order of descending diversityPercentage", function() {
-        const confListWithMissingFields = helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }, { node: { totalSpeakers: 10, numberOfWomen: 2 } }, { node: { totalSpeakers: 10, numberOfWomen: 4 } }]);
-        expect(confListWithMissingFields[0].node.diversityPercentage).toEqual(.4);
-        expect(confListWithMissingFields[1].node.diversityPercentage).toEqual(.3);
-        expect(confListWithMissingFields[2].node.diversityPercentage).toEqual(.2);
+        it("can derive the numberOfMen field", function() {
+            const confListWithMissingFields = helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }]);
+            expect(confListWithMissingFields[0].node.numberOfMen).toEqual(7);
+        });
+        
+        it("can derive the diversityPercentage field", function() {
+            const confListWithMissingFields = helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }]);
+            expect(confListWithMissingFields[0].node.diversityPercentage).toEqual(.3);
+        });
+        
+        it("can returns confs in order of descending diversityPercentage", function() {
+            const confListWithMissingFields = helper.addDerivedFields([{ node: { totalSpeakers: 10, numberOfWomen: 3 } }, { node: { totalSpeakers: 10, numberOfWomen: 2 } }, { node: { totalSpeakers: 10, numberOfWomen: 4 } }]);
+            expect(confListWithMissingFields[0].node.diversityPercentage).toEqual(.4);
+            expect(confListWithMissingFields[1].node.diversityPercentage).toEqual(.3);
+            expect(confListWithMissingFields[2].node.diversityPercentage).toEqual(.2);
+        });
     });
 
 });
