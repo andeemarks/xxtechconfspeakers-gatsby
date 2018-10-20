ce722eaa12cdc3a0c69abbae8f4e904bdae684cb
Andy Marks <vampwillow@gmail.com>
2018-10-20 22:07:44 +1100

    Oops - forgot to save

--- src/components/Charts/ChartDataFormatter.js
+++ src/components/Charts/ChartDataFormatter.js
@@ -1,17 +1,5 @@
 var _ = require('underscore')
 
-const diversityStyles = {
-  0: 'red',
-  1: 'fuchsia',
-  2: 'orange',
-  3: 'blue',
-  4: 'green',
-  5: 'white',
-  6: 'white',
-  7: 'white',
-  8: 'white',
-  9: 'white',
-}
 
 class ChartDataFormatter {
   constructor() {}
@@ -22,15 +10,14 @@ class ChartDataFormatter {
     })
 
     const formattedData = _.map(sortedConfs, function(conf, index) {
-      const diversityPercentage = conf['node']['diversityPercentage']
-      const percentageCohort = Math.floor(diversityPercentage * 10)
-
-      return {
-        color: percentageCohort,
-        y0: 0.5,
-        y: diversityPercentage,
-        x: index,
-      }
+        const diversityPercentage = conf['node']['diversityPercentage'];
+        const percentageCohort = Math.floor(diversityPercentage * 10)
+      
+          return { 
+						color: percentageCohort, 
+						y0: 0.5, 
+						y: diversityPercentage, 
+						x: index }
     })
 
     return formattedData
