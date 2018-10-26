a0945b4016b0494afd27bd28ac050fd3296e21f0
Andy Marks <vampwillow@gmail.com>
2018-10-26 13:16:19 +1100

    Added remining tests for details field values

--- spec/components/Charts/ChartDataFormatter.spec.js
+++ spec/components/Charts/ChartDataFormatter.spec.js
@@ -3,6 +3,7 @@ require('jasmine-collection-matchers');
 describe("The ChartDataFormatter module", function() {
     var ChartDataFormatter = require('../../../src/components/Charts/ChartDataFormatter');
     var formatter;
+    var chartData;
 
     beforeEach(function() {
       formatter = new ChartDataFormatter();
@@ -10,16 +11,74 @@ describe("The ChartDataFormatter module", function() {
 
     it("can return a collection of constant average datapoints matching the chart data", function() {
       const average = .25;
-      const chartData = [{index: 1, node: {confDate: "foo"}}, {index: 2, node: {confDate: "bar"}}];
+      const chartData = [{node: {confDate: "foo"}}, {node: {confDate: "bar"}}];
       const formattedData = formatter.format(chartData, average);
       expect(formattedData.average).toEqual([{x: 0, y: average}, {x: 1, y: average}]);
 
     });
 
-    it("can return a collection of chart datapoints based on the conf data", function() {
-      const chartData = [{index: 1, node: {confDate: "foo"}}];
-      const formattedData = formatter.format(chartData, .25);
-      expect(formattedData.details).toEqual([{x: 0, y: average}]);
+    describe("can return a collection of chart datapoints based on the conf data", function() {
+
+      beforeEach(function() {
+        formatter = new ChartDataFormatter();
+        chartData = [{node: {confDate: "foo", name: "name", year: 2001, location: "Paris, Texas"}}];
+      });
+
+      it("where confDate is carried forward from the chart data", function() {
+        const formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].confDate).toEqual("foo");
+      });
+
+      it("where name is carried forward from the chart data", function() {
+        const formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].name).toEqual("name");
+      });
+
+      it("where year is carried forward from the chart data", function() {
+        const formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].year).toEqual(2001);
+      });
+
+      it("where location is carried forward from the chart data", function() {
+        const formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].location).toEqual("Paris, Texas");
+      });
+
+      it("where y0 is always 0.5", function() {
+        const formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].y0).toEqual(0.5);
+      });
+
+      it("where y is the diversity percentage", function() {
+        chartData[0].node.diversityPercentage = .324;
+        const formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].y).toEqual(0.324);
+      });
+
+      it("where diversityPercentage is formatted as a %age with no precision", function() {
+        chartData[0].node.diversityPercentage = .324;
+        const formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].diversityPercentage).toEqual("32%");
+      });
+
+      it("where color is indexed against the diversity percentage", function() {
+        chartData[0].node.diversityPercentage = 0;
+        var formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].color).toEqual(0);
+
+        chartData[0].node.diversityPercentage = .5;
+        formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].color).toEqual(5);
+
+        chartData[0].node.diversityPercentage = .8342244;
+        formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].color).toEqual(8);
+      });
+
+      it("where x is corresponds to the sequential index of the chart data sorted by confDate", function() {
+        const formattedData = formatter.format(chartData, .25);
+        expect(formattedData.details[0].x).toEqual(0);
+      });
 
     });
 });
