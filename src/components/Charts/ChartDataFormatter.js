1633e48ce21fb3f82bec7b8eb58784e41279a49e
Andy Marks <vampwillow@gmail.com>
2018-10-26 13:26:06 +1100

    Refactor to split into separate functions

--- src/components/Charts/ChartDataFormatter.js
+++ src/components/Charts/ChartDataFormatter.js
@@ -4,13 +4,20 @@ var numeral = require('numeral')
 class ChartDataFormatter {
   constructor() {}
 
-  format(chartData, averageDiversity) {
-    const sortedConfs = _.sortBy(chartData, function(conf) {
-      return conf.node.confDate
-    })
+  calculateAverageDiversitySeries(chartData, averageDiversity) {
+    return [
+      { x: chartData[0].x, y: averageDiversity },
+      {
+        x: chartData[chartData.length - 1].x,
+        y: averageDiversity,
+      },
+    ]
+  }
 
-    const detailedConfData = _.map(sortedConfs, function(conf, index) {
-      const diversityPercentage = conf['node']['diversityPercentage']
+  calculateConfDetails(sortedConfs) {
+    return _.map(sortedConfs, function(currentConf, index) {
+			const conf = currentConf['node'];
+      const diversityPercentage = conf['diversityPercentage']
       const percentageCohort = Math.floor(diversityPercentage * 10)
 
       return {
@@ -19,22 +26,28 @@ class ChartDataFormatter {
         y: diversityPercentage,
         diversityPercentage: numeral(diversityPercentage).format('0%'),
         x: index,
-        confDate: conf['node']['confDate'],
-        name: conf['node']['name'],
-        year: conf['node']['year'],
-        location: conf['node']['location'],
+        confDate: conf['confDate'],
+        name: conf['name'],
+        year: conf['year'],
+        location: conf['location'],
       }
     })
+  }
 
-    const averageData = [
-      { x: detailedConfData[0].x, y: averageDiversity },
-      {
-        x: detailedConfData[detailedConfData.length - 1].x,
-        y: averageDiversity,
-      },
-    ]
+  format(chartData, averageDiversity) {
+    const sortedConfs = _.sortBy(chartData, function(conf) {
+      return conf.node.confDate
+    })
 
-    return { details: detailedConfData, average: averageData }
+    const detailedConfData = this.calculateConfDetails(sortedConfs)
+
+    return {
+      details: detailedConfData,
+      average: this.calculateAverageDiversitySeries(
+        detailedConfData,
+        averageDiversity
+      ),
+    }
   }
 }
 
