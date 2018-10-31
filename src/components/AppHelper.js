000a100e8b1acc421989efd1cfcf7e7efd8d30e5
Andy Marks <vampwillow@gmail.com>
2018-10-31 13:19:17 +1100

    Never used

--- src/components/AppHelper.js
+++ src/components/AppHelper.js
@@ -4,15 +4,6 @@ const _ = require('underscore')
 const ConfListFormatter = require('./ConfList/ConfListFormatter')
 
 class AppHelper {
-  isDataCompliantWithSchema(confs, confsSchema) {
-    var Ajv = require('ajv')
-    var schemaValidator = new Ajv()
-    schemaValidator.addMetaSchema(
-      require('ajv/lib/refs/json-schema-draft-06.json')
-    )
-
-    return schemaValidator.validate(confsSchema, confs)
-  }
 
   addDerivedFields(confs) {
     var confsWithDerivedFields = _.map(confs, function(currentConf) {
