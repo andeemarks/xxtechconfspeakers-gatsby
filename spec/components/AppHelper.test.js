5cc6b50ea981ff4c4ed495da018da5322b793893
Andy Marks <vampwillow@gmail.com>
2019-04-23 09:43:16 +1000

    First step in cutover from jasmine to ava to support ES6

--- spec/components/AppHelper.test.js
+++ spec/components/AppHelper.test.js
@@ -35,3 +35,32 @@ test('#augmentConfData handles unique diversityPercentage values', t => {
     t.is(2, rankedConfList[1].node.index)
     t.is(3, rankedConfList[2].node.index)
 });
+
+test('#augmentConfData handles duplicate diversityPercentage values', t => {
+
+    const confs = [{
+            node: {
+                totalSpeakers: 10,
+                numberOfWomen: 3
+            }
+        },
+        {
+            node: {
+                totalSpeakers: 5,
+                numberOfWomen: 2
+            }
+        },
+        {
+            node: {
+                totalSpeakers: 10,
+                numberOfWomen: 4
+            }
+        }
+    ];
+    
+    const rankedConfList = helper.augmentConfData(confs);
+
+    t.is(1, rankedConfList[0].node.index)
+    t.is("", rankedConfList[1].node.index)
+    t.is(3, rankedConfList[2].node.index)
+});
